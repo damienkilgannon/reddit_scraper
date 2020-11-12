fe64c51d51950d5ce98b181be2aeee435ecb531f
D Kilgannon <kilgannon.damien@gmail.com>
2020-11-12 14:31:37 +0000

    updates

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -8,68 +8,71 @@ import os
 from praw.models import MoreComments
 
 
-def scrape_comments(path, id, submission_comments, query):
-    fields = ['subreddit', 'parent_post', 'created', 'created_utc', 'title', 'text', 'url', 'type']
+def scrape(path, iter, query, type, id=None):
+    fields = ['subreddit', 'author', 'created_utc', 'title', 'text', 'url', 'type', 'parent_post']
+    parent_post = None
+    items = []
 
-    comments = []
-
-    for comment in submission_comments:
-        if isinstance(comment, MoreComments):
+    for item in iter:
+        if isinstance(item, MoreComments):
             continue
-        if query not in comment.body:
-            continue
-        comments.append([comment.subreddit, comment.submission, comment.author, comment.created_utc, '', comment.body, comment.permalink, 'comment'])
+        try:
+            body = item.body
+            if query not in body:
+                continue
+        except AttributeError:
+            body = ""
+        try:
+            title = item.title
+        except AttributeError:
+            title = ""
 
-    print("Number of comments: " + str(len(comments)))
+        items.append([item.subreddit, item.author, item.created_utc, title, body, item.permalink, type, id])
+
+        try:
+            if item.num_comments > 0:
+                scrape(path, item.comments, query, "comment", id=item.id)
+        except AttributeError:
+            continue
 
     if not os.path.exists(path):
         os.makedirs(path)
 
-    filename = subreddit + '_' + query + '_' + id + "_comments.csv"
+    if id:
+        filename = str(id) + "_" + type + ".csv"
+    else:
+        filename = type + ".csv"
 
     with open(os.path.join(path, filename), 'w') as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(fields)
-        csvwriter.writerows(comments)
+        csvwriter.writerows(items)
 
-    return
-
-
-def scrape(reddit, subreddit, query):
-    path = 'data/' + str(uuid.uuid1().int)[:6]
-
-    fields = ['subreddit', 'created', 'created_utc', 'title', 'text', 'url', 'type']
-
-    submissions = []
-
-    for submission in reddit.subreddit(subreddit).search(query, sort='relevance', time_filter='all', limit=None):
-        submissions.append([submission.subreddit, submission.author, submission.created_utc, submission.title, submission.selftext, submission.url, 'post'])
-        if submission.num_comments > 0:
-            scrape_comments(path, submission.id, submission.comments, query)
-
-    print("Number of posts: " + str(len(submissions)))
-
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-    filename = subreddit + '_' + query + "_posts.csv"
-
-    with open(os.path.join(path, filename), 'w') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(fields)
-        csvwriter.writerows(submissions)
-
-    return
 
 if __name__ == '__main__':
     reddit = praw.Reddit(
-        client_id = "j1oQvtLhIBbPkg",
-        client_secret = "nsw3AAYc0dOPOZ4h1jsatvJODAmAJA",
-        user_agent = "android:com.example.myredditapp:v1.2.3 (by u/kemitche)"
+        client_id = os.getenv('CLIENT_ID'),
+        client_secret = os.getenv('CLIENT_SECRET'),
+        user_agent = os.getenv('USER_AGENT', "android:com.example.myredditapp:v1.2.3 (by u/kemitche)")
     )
 
-    print('Enter subreddit >>> ', end='')
-    subreddit = input()
-    print('Enter query >>> ', end='')
+    uuid = str(uuid.uuid1().int)[:6]
+    print("Enter query term >>> ", end="")
     query = input()
-    scrape(reddit, subreddit, query)
+    print("Enter space seperated list of subreddits >>> ", end="")
+    subreddits = input().split()
+
+    print("Scraping the follow subreddits for posts and comments matching the query term.")
+    print("Subreddits: ", end="")
+    print(subreddits)
+    print("Query: ", end="")
+    print(query)
+    print("Scrape UUID: ", end="")
+    print(uuid)
+    print("Output path: ", end="")
+    print("data/" + uuid + "/<subreddit>/<query>")
+
+    for subreddit in subreddits:
+        path = 'data/' + uuid + '/' + subreddit + '/' + query
+        iter = reddit.subreddit(subreddit).search(query, sort='relevance', time_filter='all', limit=None)
+        scrape(path, iter, query, "post")
